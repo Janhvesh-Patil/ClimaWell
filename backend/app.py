27cfab52491af157f3bd9f06f9d57b24b18c8464
Janhvesh-Patil <janhveshspatil25@gmail.com>
2026-01-14 13:40:36 +0530

    Add localhost:8080 to CORS

--- backend/app.py
+++ backend/app.py
@@ -12,6 +12,7 @@ app = Flask(__name__)
 CORS(app, resources={
     r"/*": {
         "origins": [
+            "http://localhost:8080",
             "http://localhost:5173",
             "http://localhost:3000",
             "https://heat-aware-dashboard.vercel.app",
